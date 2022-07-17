bb9c8fc22f1710633b716f680206f06d44f3db19
temo <chxaidzetemo@gmail.com>
2022-07-17 21:02:36 +0400

    animal kingdom

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,18 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
 
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+    public String getDescription(){
+        String ad = hasFur?"a":"no";
+        String paw = numberOfPaws>1?"paws":"paw";
+        return "This animal is mostly "+color+". It has "+numberOfPaws+" "+paw+" and "+ad+" fur.";
+    }
 }
